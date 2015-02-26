3a9e454d4b61eca83a8ae690511e9e8632f5033d
Cole Minnaar <onslauth@gmail.com>
2015-02-26 10:43:27 +0200

    Updating daemon to log to rotating files. Fixing start script to detect already running instances.

--- src/libexec/instance_start.sh
+++ src/libexec/instance_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "starting pq-file-io..."
+echo "Starting pq-file-io..."
 
 if test -z "$CHASSIS_VAR_DIR"
 then
@@ -19,13 +19,23 @@ chassis_var_dir=$CHASSIS_VAR_DIR
 part_dir=$PART_DIR
 instance_name=$INSTANCE_NAME
 
-echo "chasssis_var_dir=[$chassis_var_dir]"
-echo "part_dir=[$part_dir]"
-echo "instance_name=[$instance_name]"
-echo "pwd=[$PWD]"
-echo "command=[$command]"
-echo "PATH=[$PATH]"
-echo "LD_LIBRARY_PATH=[$LD_LIBRARY_PATH]"
+#echo "chasssis_var_dir=[$chassis_var_dir]"
+#echo "part_dir=[$part_dir]"
+#echo "instance_name=[$instance_name]"
+#echo "pwd=[$PWD]"
+#echo "command=[$command]"
+#echo "PATH=[$PATH]"
+#echo "LD_LIBRARY_PATH=[$LD_LIBRARY_PATH]"
+
+if test -f ./pq-file-io.pid
+then
+	pid=$( cat ./pq-file-io.pid )
+	if kill -0 $pid 2>/dev/null
+	then
+		printf "File-IO is already running.\n"
+		exit 0
+	fi
+fi
 
 "$part_dir/libexec/file-io-daemon" 2>&1 1>../log/file-io-daemon.log &
 echo $! > pq-file-io.pid
